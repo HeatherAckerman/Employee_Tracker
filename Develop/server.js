50cf435bf20bf451b2fef84144a2f28598e72daf
HeatherAckerman <hla20115213@gmail.com>
2020-10-26 16:20:42 -0500

    Psudocode

---
+++ Develop/server.js
@@ -0,0 +1,40 @@
+//Require inquirer and sql
+
+//Create a connection
+
+//Create 'home' page where the user can make a choice between
+//View departments, roles, employees
+//Adding departments, roles, employees
+//Update employee rolls
+
+//Use a switch case to start the function dependant on what the user chose
+
+//Functions run by the switch case
+//Prompts in functions to get info
+function viewEmployees() {
+
+}
+
+function viewEmpByDepart() {
+
+}
+
+function viewEmpByRole() {
+    
+}
+
+function addEmployee() {
+    
+}
+
+function addEmpByDepart() {
+    
+}
+
+function addEmpByRole() {
+    
+}
+
+function updateEmpRoles() {
+    
+}
